d78cf1803e6a9c0b5eb3e5cd5d1fb32a019e01a0
oddg <odegevigney@gmail.com>
2017-08-05 23:40:41 -0700

    Move validation out of algo

--- algo.go
+++ algo.go
@@ -1,31 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
-
-func validate(costs [][]int) error {
-	n := len(costs)
-
-	if n == 0 {
-		return errors.New("The costs matrix is empty.")
-	}
-
-	if m := len(costs[0]); m != n {
-		return errors.New("The costs matrix is not square.")
-	}
-
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if costs[i][j] < 0 {
-				return fmt.Errorf("The coefficient (%d,%d) is negative.", i, j)
-			}
-		}
-	}
-
-	return nil
-}
 
 // Returns the solution as an array `a` such that each row `i` is matched to
 // column `a[i]`
